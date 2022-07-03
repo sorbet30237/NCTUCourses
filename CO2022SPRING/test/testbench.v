6c807dfb84f0698fff56fd78b248e2c99a030122
sorbet30237 <71446764+sorbet30237@users.noreply.github.com>
2022-07-03 15:34:20 +0800

    u

--- CO2022SPRING/test/testbench.v
+++ CO2022SPRING/test/testbench.v
@@ -44,7 +44,7 @@ initial begin
         cpu.IM.instruction_file[i] = 32'b0;
     end
 
-    $readmemb("CO_P5_test_2.txt", cpu.IM.instruction_file);  //Read instruction from "CO_P4_test_1.txt"   
+    $readmemb("CO_P5_test_1.txt", cpu.IM.instruction_file);  //Read instruction from "CO_P4_test_1.txt"   
     
     // data memory
     for(i=0; i<128; i=i+1)
